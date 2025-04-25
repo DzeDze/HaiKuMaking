61984beae8e02fc1df2ea5f8bd8c0bb27ef3eaad
Phuc Nguyen <phuc.nguyen.ufo@gmail.com>
2025-04-25 17:36:19 -0400

    Initial Commit

---
+++ Haiku/HaikuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaikuApp.swift
+//  Haiku
+//
+//  Created by Vince P. Nguyen on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct HaikuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
